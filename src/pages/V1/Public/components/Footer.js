606289242abd1ed0b224aa8787e08f35ee3ed632
肖爽 <xiao.shuang@qianka.com>
2019-04-22 15:49:21 +0800

    页面底部

--- src/pages/V1/Public/components/Footer.js
+++ src/pages/V1/Public/components/Footer.js
@@ -1,4 +1,5 @@
 import React, { PureComponent } from 'react';
+import Link from 'umi/link';
 import styles from './Footer.less';
 
 class Footer extends PureComponent {
@@ -11,11 +12,54 @@ class Footer extends PureComponent {
           <div className={styles.list}>
             <div className={styles.item}>
               <div className={styles.title}>超多客</div>
-              <ul>
-                <li>1111</li>
-              </ul>
+              <Link className={styles.ele} to="/">
+                公司简介
+              </Link>
+              <Link className={styles.ele} to="/">
+                企业文化
+              </Link>
+              <Link className={styles.ele} to="/">
+                合作联系
+              </Link>
+            </div>
+            <div className={styles.item}>
+              <div className={styles.title}>产品服务</div>
+              <Link className={styles.ele} to="/">
+                CMS网站
+              </Link>
+              <Link className={styles.ele} to="/">
+                微信小程序
+              </Link>
+              <Link className={styles.ele} to="/">
+                精品单页
+              </Link>
+              <Link className={styles.ele} to="/">
+                开放API
+              </Link>
+            </div>
+            <div className={styles.item}>
+              <div className={styles.title}>帮助支持</div>
+              <Link className={styles.ele} to="/">
+                新手入门
+              </Link>
+              <Link className={styles.ele} to="/">
+                常见问题
+              </Link>
+            </div>
+            <div className={styles.item}>
+              <div className={styles.title}>微信服务号</div>
+              <div className={styles.eleImg}>
+                <img
+                  src="https://cdn.youlianyc.com/image/static/2f94f6c6051e322b7f5bac11ae8d5c7df4263526.jpg"
+                  alt=""
+                />
+              </div>
             </div>
           </div>
+        </div>
+        <div className={styles.footer_below}>
+          Copyright © 2018<span className={styles.comp}> 极单信息科技有限公司 </span>
+          沪ICP备17028167号-6 版权所有
         </div>
       </div>
     );
