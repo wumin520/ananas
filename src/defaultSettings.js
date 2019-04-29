e3c334032e0b069fdd5e086d74d6f771683188f4
肖爽 <xiao.shuang@qianka.com>
2019-04-29 18:25:20 +0800

    production

--- src/defaultSettings.js
+++ src/defaultSettings.js
@@ -16,6 +16,6 @@ module.exports = {
   // 注意：如果需要图标多色，Iconfont 图标项目里要进行批量去色处理
   // Usage: https://github.com/ant-design/ant-design-pro/pull/3517
   iconfontUrl: '',
-  // backend: 'https://chaoduoke.com',
-  backend: 'http://test.chaoduoke.com',
+  backend: 'https://chaoduoke.com',
+  // backend: 'http://test.chaoduoke.com',
 };
