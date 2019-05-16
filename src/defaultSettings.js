31f3cc295ea36c5223f59c92dec2af9445cdfad9
肖爽 <xiao.shuang@qianka.com>
2019-05-16 11:33:06 +0800

    test.fans

--- src/defaultSettings.js
+++ src/defaultSettings.js
@@ -22,7 +22,8 @@ module.exports = {
   configs: {
     test: {
       // API_SERVER: 'http://test.chaoduoke.com', // 测试环境
-      API_SERVER: 'http://temp_fix.test.chaoduoke.com', // 测试环境
+      // API_SERVER: 'http://temp_fix.test.chaoduoke.com',
+      API_SERVER: 'http://fan.test.chaoduoke.com',
     },
     production: {
       API_SERVER: 'https://chaoduoke.com', // 线上环境
